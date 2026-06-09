51677ff62a0adcd0c89943d62457cc3d3db97235
Hoyostools <soporte@hoyostools.com>
2026-06-09 09:39:53 -0500

    feat: agregar campo warehouse_id en stock.picking y actualizar vista correspondiente

--- campos_hoyos/models/stock_picking.py
+++ campos_hoyos/models/stock_picking.py
@@ -7,3 +7,10 @@ class StockPicking(models.Model):
     address_complete = fields.Char(related='partner_id.street')
     carrier_id_name = fields.Char(related='carrier_id.name')
     packaging_order_observation = fields.Text(string='Totalidad del empaque de la orden', related='sale_id.packaging_order_observation', store=True, readonly=False,)
+    warehouse_id = fields.Many2one(
+        comodel_name="stock.warehouse",
+        string="Almacén",
+        related="picking_type_id.warehouse_id",
+        store=True,
+        readonly=True,
+    )
