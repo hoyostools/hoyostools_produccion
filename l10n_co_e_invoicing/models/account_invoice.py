76fb1b778de78d75a12ce6868939a934c5e4f6c0
estivencastro <ingenieria2@hoyostools.com>
2026-06-10 13:41:29 -0500

    fix

--- l10n_co_e_invoicing/models/account_invoice.py
+++ l10n_co_e_invoicing/models/account_invoice.py
@@ -261,9 +261,9 @@ class AccountInvoice(models.Model):
                         type_account = 'credit'
                     else:
                         type_account = 'invoice'
-                    dian_document_obj = self.env['account.invoice.dian.document'].search([(('invoice_id','=', record.id),('company_id','=',record.company_id.id),('type_account','=',type_account))])
+                    dian_document_obj = self.env['account.invoice.dian.document']
                     dian_document = self.env['account.invoice.dian.document'].search([(('invoice_id','=', record.id),('company_id','=',record.company_id.id),('type_account','=',type_account))])
-                    if not dian_document_obj:
+                    if not dian_document:
                         dian_document = dian_document_obj.create({
                             'invoice_id': record.id,
                             'company_id': record.company_id.id,
